ca96756995fa869cf2cc83d30950c6fc63589013
creye111 <car168490@gmail.com>
2021-05-15 19:59:45 -0400

    started merge sorted array

---
+++ src/algoprac/MergeSortedArray.java
@@ -0,0 +1,16 @@
+package algoprac;
+
+public class MergeSortedArray {
+	 public void merge(int[] nums1, int m, int[] nums2, int n) {
+	        int c1 =m-1;
+	        int c2=n-1;
+	        while(c1>0&&c2>0){
+	            if(nums1[c1]>nums2[c2]){
+	               nums1[c1+c2-1] = nums[c1];
+	                c1--;
+	            }else{
+	                
+	            }
+	        }
+	        
+}
