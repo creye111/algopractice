6c832dfc37cf3fe23b9a1386f91e189c8fcf848b
creye111 <car168490@gmail.com>
2021-05-17 21:37:50 -0400

    x

--- src/algoprac/MergeSortedArray.java
+++ src/algoprac/MergeSortedArray.java
@@ -6,11 +6,15 @@ public class MergeSortedArray {
 	        int c2=n-1;
 	        while(c1>0&&c2>0){
 	            if(nums1[c1]>nums2[c2]){
-	               nums1[c1+c2-1] = nums[c1];
+	               nums1[c1+c2-1] = nums1[c1];
 	                c1--;
 	            }else{
-	                
+	            	nums1[c1+c2-1] = nums2[c2];
+	            	c2--;
 	            }
 	        }
 	        
+	        while(c2>0) {
+	        	if(nums1)
+	        }
 }
