1b87ea36150ccc72c5f125561f2df6ae04ce4c7c
creye111 <car168490@gmail.com>
2021-06-16 19:30:16 -0400

    53. implemented solution

--- src/algoprac/MaxContiguousSubSequenceSum.java
+++ src/algoprac/MaxContiguousSubSequenceSum.java
@@ -5,12 +5,18 @@ public class MaxContiguousSubSequenceSum {
 		
 	}
 	private static int maxSumRecLog(int []a, int left, int right) {
+		
+		//Base case: if left=right, and there is one element:
+		//				it is the max subsequence if the element is nonnegative
+		//				
 		if(left==right)
 			if(a[left]>0)
 				return a[left];
 			else
 				return 0;
 		int center = (left + right)/2;
+		//divide the array into two sub problems and calculate the max left sum, right sum and the sum of both.
+		// then find the max of both
 		int maxLeftSum = maxSumRecLog(a,left,center);
 		int maxRightSum = maxSumRecLog(a,center+1,right);
 		
