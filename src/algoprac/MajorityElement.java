0a7bd1ca102e58fb97ef1304c2893ca2f035ffa9
creye111 <car168490@gmail.com>
2021-06-24 21:19:59 -0400

    accepted majority element solution

---
+++ src/algoprac/MajorityElement.java
@@ -0,0 +1,26 @@
+package algoprac;
+
+public class MajorityElement {
+	    public int majorityElement(int[] nums) {
+	        HashMap <Integer,Integer> map = new HashMap(nums.length/2);
+	        int temp;
+	        int maxKey=nums[0];
+	        int maxVal =1;
+	        for(int  n: nums){
+	            if(map.get(n)!=null){
+	                temp= map.get(n);
+	                map.replace(n,temp+1);
+	                if(temp+1>maxVal){
+	                    maxKey = n;
+	                    maxVal = temp+1;
+	                }
+	            }else{
+	                map.put(n,1);
+	            }
+	        }
+	        return maxKey;
+	        
+	        
+	    }
+	}
+}
